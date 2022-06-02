f7925c25a4f3da2ab16d9fb65779fabe716d2a10
hyungkiShin <tkaqkeldk@nate.com>
2022-06-02 23:43:31 +0900

    feat crud test Add

--- src/test/java/proj/jdbc/repository/MemberRepositoryV0Test.java
+++ src/test/java/proj/jdbc/repository/MemberRepositoryV0Test.java
@@ -1,5 +1,6 @@
 package proj.jdbc.repository;
 
+import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -7,6 +8,7 @@ import proj.jdbc.domain.Member;
 
 import java.sql.SQLException;
 
+@Slf4j
 public class MemberRepositoryV0Test {
 
    MemberRepositoryV0 repository = new MemberRepositoryV0();
@@ -17,5 +19,12 @@ public class MemberRepositoryV0Test {
        // given
        final Member member = new Member("memberV1", 10000);
        repository.save(member);
+
+       // find By Id
+       final Member findMember = repository.findById(member.getMemberId());
+       log.info("findmember= {}", findMember);
+
+       // 내가 비교 하려는것
+       Assertions.assertThat(findMember).isEqualTo(member);
    }
 }
