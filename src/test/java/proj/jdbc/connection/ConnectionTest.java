c0ab8c64e27862998881a6bea397987aad4165d0
hyungkiShin <tkaqkeldk@nate.com>
2022-06-14 00:01:10 +0900

    [refactor] assets
    이미지 폴더 생성후 docs image 정리

--- src/test/java/proj/jdbc/connection/ConnectionTest.java
+++ src/test/java/proj/jdbc/connection/ConnectionTest.java
@@ -4,7 +4,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
+import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -13,7 +15,7 @@ import static proj.jdbc.connection.ConnectionConst.*;
 
 @Slf4j
 public class ConnectionTest {
-    
+
     @Test
     @DisplayName("DriverManager")
     void DriverManager() throws SQLException {
@@ -21,5 +23,21 @@ public class ConnectionTest {
         final Connection con2 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
         log.info("connection={} class={}", con1, con1.getClass());
         log.info("connection={} class={}", con2, con2.getClass());
+    }
+
+    @Test
+    @DisplayName("DriverManager")
+    void DriverSourceDataManager() throws SQLException {
+        DataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        useDataSource(dataSource);
+    }
+
+
+    void useDataSource(DataSource dataSource) throws SQLException {
+        final Connection con1 = dataSource.getConnection();
+        final Connection con2 = dataSource.getConnection();
+
+        log.info("con1={}, class={}", con1, con1.getClass());
+        log.info("con2={}, class={}", con2, con2.getClass());
     }
 }
